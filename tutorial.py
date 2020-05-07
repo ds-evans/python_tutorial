f19dc00850edacf925e753ca5f960f4169eb68ba
David Evans <david.evans1209@gmail.com>
2020-05-07 19:36:58 -0400

    Completed string introduction and started the numbers introduction

--- tutorial.py
+++ tutorial.py
@@ -1,6 +1,19 @@
 # An introductory tutorial to python
-# String introduction
 
+# String introduction
 character_name = "John"
 character_age = 35
 print("There once was a man, " + character_name + ", who was " + str(character_age));
+phrase = "Python Academy"
+print(phrase + " is cool")
+print(phrase.lower().upper().isupper())
+print(phrase[0])
+print(phrase[1])
+print(phrase.index("A"))
+print(phrase.replace("Python", "Snake"))
+
+# Number introduction
+my_number = 5
+print("\n" + str(my_number))
+print(3 * (4 + 5))
+print(10 % 3)
