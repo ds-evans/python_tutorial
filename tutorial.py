233471a95bac12c9f854bd8bac510689bcc36f79
David Evans <david.evans1209@gmail.com>
2020-05-07 19:27:17 -0400

    Initial Commit - Simple python tutorial code along

---
+++ tutorial.py
@@ -0,0 +1,6 @@
+# An introductory tutorial to python
+# String introduction
+
+character_name = "John"
+character_age = 35
+print("There once was a man, " + character_name + ", who was " + str(character_age));
